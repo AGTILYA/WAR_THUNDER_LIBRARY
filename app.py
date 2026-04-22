b6baeecd5472b83f7f6c5fe2a860180a2c922120
AGTILYA <mvsilov76@mail.ru>
2026-04-22 21:28:14 +0700

    Мы убряли стряку. И все :)

--- app.py
+++ app.py
@@ -439,4 +439,3 @@ if __name__ == '__main__':
     db.init_database()
     print("\n=== War Thunder Коллекция ===")
     print("Сервер запущен на http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
